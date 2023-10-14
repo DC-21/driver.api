1d01a0860f3ca4d3d810b8bc28a9816b23e1e34a
DC-21 <cholahkuboko@gmail.com>
2023-10-14 14:53:50 +0200

    first commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using UserAuth.Api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddEntityFrameworkNpgsql().AddDbContext<ApiDbContext>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Install Postgresql
 dotnet add package Npgsql.EntityFrameworkCore.postgreSQL --version 7.0.11
 dotnet add package Microsoft.EntityFrameworkCore.Design --version 7.0.12
 dotnet add package Microsoft.EntityFrameworkCore.Tools --version 7.0.11
+
+create Folder Data in root directory
+create class APiDbContext in Data and inherit its properties from DbContext
+embed DbContext and connection string in program.cs
+
+run : dotnet build to check for error and if it succeeds in build process then its to start building the models.
