6868b7a7f796adfbb42820324eb27db15c675fa9
DC-21 <cholahkuboko@gmail.com>
2023-10-14 15:04:14 +0200

    first commit

---
+++ Models/BaseEntity.cs
@@ -0,0 +1,9 @@
+namespace UserAuth.Api.Models;
+
+public class BaseEntity
+{
+    public int Id { get; set; }
+    public int Status { get; set; }
+    public DateTime DateAdded { get; set; }
+    public DateTime DateUpdated { get; set; }
+}
---
+++ Models/Team.cs
@@ -0,0 +1,7 @@
+namespace UserAuth.Api.Models;
+
+public class Team: BaseEntity
+{
+    public string Name { get; set; } = "";
+    public int Year { get; set; } = 2022;
+}
